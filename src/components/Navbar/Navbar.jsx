9f5a8e8f98ffc641f27b33ff8b5e625865f1eddb
OsirisFer <88387319+OsirisFer@users.noreply.github.com>
2024-06-06 01:32:30 -0300

    fix: Fix typo in Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,9 +9,9 @@ export const Navbar = () => {
   
     return (
       <nav className={styles.navbar}>
-        <span className={styles.title}>
+        <a className={styles.title} href="#about">
           Portfolio
-        </span>
+        </a>
         <div className={styles.menu}>
           <img
             className={styles.menuBtn}
