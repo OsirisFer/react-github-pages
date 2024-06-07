a74fa64f0ce7cbfd15cf1cc4a9faf73477a38c21
OsirisFer <88387319+OsirisFer@users.noreply.github.com>
2024-06-07 01:09:25 -0300

    Contact Component Changed +

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-
 import styles from "./Contact.module.css";
-import emailIcon from "./ContactImages/emailIcon.png";
-import linkedinIcon from "./ContactImages/linkedinIcon.png";
-import githubIcon from "./ContactImages/githubIcon.png";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./SocialLinks.css";
 
@@ -14,23 +10,24 @@ export const Contact = () => {
         <h2>Contact</h2>
         <p>Feel free to reach out!</p>
       </div>
-      <ul className={styles.links}>
-        <li className={styles.link}>
-          <img src={emailIcon} alt="Email icon" />
-          <a href="mailto:osirisfernandez1999@gmail.com">osirisfernandez1999@gmail.com</a>
+      <ul className="socialLinks">
+        <li>
+          <a href="mailto:osirisfernandez1999@gmail.com">
+            <i className="fas fa-envelope icon"></i>
+          </a>
         </li>
-        <li className={styles.link}>
-          <img
-            src={linkedinIcon}
-            alt="LinkedIn icon"
-          />
-          <a href="https://www.linkedin.com/in/osiris-fern%C3%A1ndez-515095217">linkedin.com/osiris-fernández</a>
+        <li>
+          <a href="https://www.linkedin.com/in/osiris-fern%C3%A1ndez-515095217">
+            <i className="fab fa-linkedin-in icon"></i>
+          </a>
         </li>
-        <li className={styles.link}>
-          <img src={githubIcon} alt="Github icon" />
-          <a href="https://github.com/OsirisFer">github.com/OsirisFer</a>
+        <li>
+          <a href="https://github.com/OsirisFer">
+            <i className="fab fa-github icon"></i>
+          </a>
         </li>
       </ul>
     </footer>
   );
 };
+
